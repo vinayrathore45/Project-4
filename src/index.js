ebcaa1fe703ac248d7c8f3de41aa9c9839c6147c
awaizkhanmd <awaizkhanmd@gmail.com>
2022-07-18 12:28:29 +0530

    project-4

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const app = express();
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
+
+try{
+    mongoose.connect("mongodb+srv://vinayrathore45:vinay123@cluster0.euhc6tm.mongodb.net/group13Database",{useNewUrlParser:true})
+    console.log("MongoDB is connected")
+}
+catch (error){
+    console.log(error)
+}
+//app.use("/",route)
+app.listen(3000, console.log('Express app running on port 3000'))
