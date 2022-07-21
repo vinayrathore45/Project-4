76e6817a5e72b5cd2f3195d3f2684f5b5dd8993f
Souvik Jha <jhasouvik4@gmail.com>
2022-07-22 00:49:42 +0530

    success

--- src/index.js
+++ src/index.js
@@ -18,4 +18,10 @@ catch (error){
     console.log(error)
 }
 app.use("/",route)
+
+app.use((req,res,next)=>{
+    res.status(404).send({status:404, msg:`Not found ${req.url}`})
+    next()
+})
+
 app.listen(3000, console.log('Express app running on port 3000'))
